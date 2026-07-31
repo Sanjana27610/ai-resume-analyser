844eefb6ce40b54b7d4503866532028bdf892ae5
Sanjana Kohli <sanjanakohli539@gmail.com>
2026-07-31 13:27:20 +0530

    Completed frontend and backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,13 @@
 import express from "express";
 import cors from "cors";
-import multer from "multer";
-import fs from "fs";
-import pdfParse from "pdf-parse";
 
 const app = express();
 
 app.use(cors());
-
-const upload = multer({ dest: "uploads/" });
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
-});
-
-app.post("/upload", upload.single("resume"), async (req, res) => {
-  try {
-    const dataBuffer = fs.readFileSync(req.file.path);
-    const data = await pdfParse(dataBuffer);
-
-    fs.unlinkSync(req.file.path);
-
-    res.json({
-      message: "Resume uploaded successfully!",
-      text: data.text,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Error reading PDF",
-    });
-  }
 });
 
 app.listen(5000, () => {
